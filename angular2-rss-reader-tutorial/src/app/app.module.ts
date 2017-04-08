b95931c8abe18fc0908d7da8bae93f996d3284e7
Kenneth Yates <ken.kyates@gmail.com>
2017-04-08 13:15:06 -0400

    Added more pieces from the tutorial

--- angular2-rss-reader-tutorial/src/app/app.module.ts
+++ angular2-rss-reader-tutorial/src/app/app.module.ts
@@ -3,18 +3,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Material design.
+import { MdCardModule } from '@angular2-material/card';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
+
 import { AppComponent } from './app.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { FeedService } from './feed-service.service';
+import { StripHtmlTagsPipe } from './pipe/strip-html-tags.pipe';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent,
+    StripHtmlTagsPipe,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdIconModule
   ],
-  providers: [],
+  providers: [FeedService, MdIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
